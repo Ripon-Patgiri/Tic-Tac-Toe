b698776659fd0a45ccf126e6e2caf4a9c7859196
Ripon-Patgiri <riponmongzeell@gmail.com>
2022-10-18 18:53:12 +0530

    setBoard and getBoard Function to manipulate Board.

--- scripts.js
+++ scripts.js
@@ -1,17 +1,27 @@
-"use strict"
+"use strict";
 
-// Player Object to Manage Player Symbol 
+// Player Object to Manage Player Symbol
 const Player = (symbol) => {
-    this.symbol = symbol;
+  this.symbol = symbol;
 
-    const getSymbol = () => {
-        return symbol;
-    };
+  const getSymbol = () => {
+    return symbol;
+  };
 
-    return { getSymbol };
-}
+  return { getSymbol };
+};
 
 // gameBoard Object to till deal with Tic Tac Toe Board
-const gameBoard = (() => {
-    const board = ["", "", "", "", "", "", "", "", ""];
-})
+const gameBoard = () => {
+  const board = ["", "", "", "", "", "", "", "", ""];
+
+  const setBoard = (index, symbol) => {
+    if (index > board.length) return;
+    board[index] = symbol;
+  };
+
+  const getBoard = (index) => {
+    if (index > board.length) return;
+    return board[index];
+  };
+};
