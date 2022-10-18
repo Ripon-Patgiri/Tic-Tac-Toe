ec6de2dc8f0fd51d17263103251006f50ddc138a
Ripon-Patgiri <riponmongzeell@gmail.com>
2022-10-18 18:45:20 +0530

    Player Object to Manage Player Symbol.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,16 @@
 "use strict"
 
+// Player Object to Manage Player Symbol 
+const Player = (symbol) => {
+    this.symbol = symbol;
+
+    const getSymbol = () => {
+        return symbol;
+    };
+
+    return { getSymbol };
+}
+
 // gameBoard Object to till deal with Tic Tac Toe Board
 const gameBoard = (() => {
     const board = ["", "", "", "", "", "", "", "", ""];
